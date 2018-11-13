0dde56dccde606a4a5c37b7e638585eaba5c9fcf
JiaxiLee <jiaxiwuli@126.com>
2018-11-13 13:58:31 +0800

    增加了spring注解的事务管理

--- src/main/java/com/lee/qiniu/service/IArticleService.java
+++ src/main/java/com/lee/qiniu/service/IArticleService.java
@@ -1,12 +1,10 @@
 package com.lee.qiniu.service;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import com.lee.qiniu.entity.Article;
 import com.lee.qiniu.json.JsonResult;
 
 public interface IArticleService {
 	
-	@Transactional(rollbackForClassName={"RollBackException"})
+	
 	JsonResult save(Article article);
 }
---
+++ src/main/java/com/lee/qiniu/service/impl/ArticleService.java
@@ -0,0 +1,34 @@
+package com.lee.qiniu.service.impl;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lee.qiniu.dao.ArticleDao;
+import com.lee.qiniu.entity.Article;
+import com.lee.qiniu.exception.RollBackException;
+import com.lee.qiniu.json.JsonResult;
+import com.lee.qiniu.service.IArticleService;
+
+@Service
+public class ArticleService implements IArticleService {
+	
+	@Autowired
+	private ArticleDao articleDao;
+	
+	@Override
+	@Transactional(rollbackForClassName={"RollBackException"})
+	public JsonResult save(Article article){
+		try {
+			article.setCreateTime(new Date());
+			articleDao.insert(article);
+			System.out.println(1/0);
+		} catch (Exception e) {
+			throw new RollBackException(e.getMessage());
+		}
+		return JsonResult.ok();
+	}
+
+}
